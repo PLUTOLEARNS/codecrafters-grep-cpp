5b9867027ad61743ff0f9725356b31fc74823a64
titantheven@gmail.com <titantheven@gmail.com>
2024-09-27 20:21:58 +0530

    trial start of str anchor chracter groups

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,67 +7,70 @@ bool match_group(const string& input_line, const string& pattern) {
     return input_line.find_first_of(pattern) != string::npos;
 }
 
-bool match_pattern(const std::string& input_line, const std::string& pattern) {
+bool match_pattern(const std::string& input_line, const std::string& pattern, bool anchored = false) {
     size_t inp_len = input_line.size();
     size_t patt_len = pattern.size();
 
     if (patt_len == 0) return true;
     if (inp_len == 0) return false;
+
+    // Handle '^' anchor
     if (pattern[0] == '^') {
-        if (patt_len == 1) { // Pattern is just "^"
-            return false; // Empty input doesn't match "^"
-        }
-        if (input_line[0] != pattern[1]) {
-            return false;
-        }
-        return match_pattern(input_line.substr(1),pattern.substr(1)); 
+        // Force pattern to match only at the beginning of input_line
+        return match_pattern(input_line, pattern.substr(1), true);
     }
+
+    if (anchored && input_line.empty()) {
+        return false;  // In case the input ends early when anchored
+    }
+
     if (pattern.substr(0, 2) == "\\d") {
         if (isdigit(input_line[0])) {
-            return match_pattern(input_line.substr(1), pattern.substr(2));
+            return match_pattern(input_line.substr(1), pattern.substr(2), anchored);
         }
-        return match_pattern(input_line.substr(1), pattern);
+        return false;
     } else if (pattern.substr(0, 2) == "\\w") {
         if (isalnum(input_line[0])) {
-            return match_pattern(input_line.substr(1), pattern.substr(2));
+            return match_pattern(input_line.substr(1), pattern.substr(2), anchored);
         }
-        return match_pattern(input_line.substr(1), pattern);
+        return false;
     } else if (pattern[0] == '[') {
         auto first = pattern.find(']');
         bool neg = pattern[1] == '^';
         if (neg) {
             if (!match_group(input_line, pattern.substr(2, first - 2))) {
-                return match_pattern(input_line.substr(1), pattern.substr(first + 1));
+                return match_pattern(input_line.substr(1), pattern.substr(first + 1), anchored);
             }
             return false;
         }
         if (match_group(input_line, pattern.substr(1, first - 1))) {
-            return match_pattern(input_line.substr(1), pattern.substr(first + 1));
+            return match_pattern(input_line.substr(1), pattern.substr(first + 1), anchored);
         } else {
             return false;
         }
     }
 
     if (pattern[0] == input_line[0]) {
-        return match_pattern(input_line.substr(1), pattern.substr(1));
+        return match_pattern(input_line.substr(1), pattern.substr(1), anchored);
     }
 
-    return match_pattern(input_line.substr(1), pattern);
+    return false;  // If the characters don't match
 }
 
-bool match_patterns(string input_line, string pattern) {
-	if (pattern[0] == '^'){
-	    return match_pattern(input_line, pattern.substr(1));
-}
-    do {
-        if (match_pattern(input_line, pattern)) {
+bool match_patterns(const std::string& input_line, const std::string& pattern) {
+    if (pattern[0] == '^') {
+        // Match at the beginning of the line (anchored)
+        return match_pattern(input_line, pattern);
+    }
+
+    // Otherwise, search the pattern throughout the input string
+    for (size_t i = 0; i < input_line.size(); ++i) {
+        if (match_pattern(input_line.substr(i), pattern)) {
             return true;
         }
-        input_line = input_line.substr(1);  // move 1 step further
-    } while (input_line != "");
+    }
     return false;
 }
-
 
 int main(int argc, char* argv[]) {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
