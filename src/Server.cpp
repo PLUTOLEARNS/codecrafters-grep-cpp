0315aea87ed5ff1da93659804dc3f85b5cf22bbf
titantheven@gmail.com <titantheven@gmail.com>
2024-09-27 19:15:57 +0530

    trial combining chracter groups

--- src/Server.cpp
+++ src/Server.cpp
@@ -57,11 +57,26 @@ bool match_patterns(string input_line, string pattern){
     return 0;
 }
 int main(int argc, char* argv[]) {
-Expand 19 lines
+    // You can use print statements as follows for debugging, they'll be visible when running tests.
+    std::cout << "Logs from your program will appear here" << std::endl;
+    if (argc != 3) {
+        std::cerr << "Expected two arguments" << std::endl;
+        return 1;
+    }
+    std::string flag = argv[1];
+    std::string pattern = argv[2];
+    if (flag != "-E") {
+        std::cerr << "Expected first argument to be '-E'" << std::endl;
+        return 1;
+    }
+    // Uncomment this block to pass the first stage
+    
+    std::string input_line;
     std::getline(std::cin, input_line);
     
     try {
         if (match_pattern(input_line, pattern)) {
+        if (match_patterns(input_line, pattern)) {
             return 0;
         } else {
             return 1;
